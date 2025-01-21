87c29cc83a4dbe5a610b32ce07b046fbb1007efd
JayangaPabasara <jayangapabasara71@gmail.com>
2025-01-21 09:38:02 +0530

    Create Authentications and Autharization. 🔒

--- controller/productController.js
+++ controller/productController.js
@@ -1,6 +1,21 @@
 import Product from "../model/Product.js";
 
 export function addProduct(req, res){
+
+    if(req.user == null){
+        res.status(401).json({
+            massage : "Please login and try again"
+        })
+        return
+    }
+
+    if(req.user.role != 'admin'){
+        res.status(403).json({
+            massage : "Your not authorized to perform this operation"
+        });
+        return
+    }
+
     const productData = req.body;
 
     const newProduct = new Product(productData);
--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import userRouter from "./route/userRoute.js";
+import productRouter from "./route/productRouter.js";
+import jwt, { decode } from "jsonwebtoken";
 
 let app = express();
 
@@ -12,13 +14,30 @@ let dbUrl =
   "mongodb+srv://jayangapabasara71:HKjBL3RWYLO1cFMJ@cluster0.ex7ii.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 mongoose.connect(dbUrl);
-  
+
 let connection = mongoose.connection;
 connection.once("open", () => {
   console.log("MongoDB connection estabilished succefuly!");
 }); //end of database connetion code
 
+//Authorization with jwt token
+app.use((req, res, next) => {
+  let token = req.header("Authorization");
+
+  if (token != null) {
+    token = token.replace("Bearer ", "");
+
+    jwt.verify(token, "kv-secret-89", (err, decoded) => {
+      if (!err) {
+        req.user = decoded;
+      }
+    });
+  }
+  next();
+});
+
 app.use("/api/user", userRouter);
+app.use("/api/product", productRouter);
 
 app.listen(3000, () => {
   console.log("Server started at 3000");
